c9ceb92beda1a1ce8d53c210464a86b0366e1103
Dawid Pawlik <dp450244@students.mimuw.edu.pl>
2024-03-03 19:46:45 +0100

    Add main page lab1

--- LAB1/scraper.py
+++ LAB1/scraper.py
@@ -1,46 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
+from duckduckgo_search import DDGS
+
+TEAM = 'G2 Esports'
+TEAM_CODE = 'g2'
+URL = f'https://lol.fandom.com/wiki/{TEAM}'
 
 
-URL = 'https://www.favikon.com/blog/the-20-most-famous-tiktok-influencers-in-the-world'
-
-html_page = requests.get(URL).content
-soup = BeautifulSoup(html_page, 'html.parser')
-
-title = soup.title.text
-title = title[:title.find('!')] + '.'
-
-list = soup.find('div', attrs='tablecontent').find_all('li')
-
-top20 = [item.text for item in list]
-
-for item in top20:
-    print(item)
-
-links = [link['href'] if link else None for link in [paragraph.find('a') for paragraph in soup.find_all('p')]]
-
-for link in links:
-    if link and 'www.tiktok.com' in link:
-        print(link)
-
-links = soup.find_all('div', attrs=['w-embed w-script'])
-
-for link in links:
-    print(link.find('a')['href'])
-
-# with open('soup.html', 'w') as file:
-#     file.write(soup.prettify())
-
-# paragraphs = soup.select('p')
-
-# for paragraph in paragraphs:
-#     print(paragraph)
+def front_matter(file, title, permalink, redirect = ''):
+    file.write('---\n')
+    file.write(f'title: {title}\n')
+    file.write(f'permalink: {permalink}\n')
+    if redirect:
+        file.write(f'redirect_from: {redirect}\n')
+    file.write('---\n')
 
 
+if __name__ == '__main__':
 
+    html_page = requests.get(URL).content
+    soup = BeautifulSoup(html_page, 'html.parser')
 
+    desc = next(DDGS().answers(TEAM))['text']
 
-with open('tiktok.md', 'w') as page:
-    page.write(f'# {title}\n')
-    for item in top20:
-        page.write(f'- [{item}]({URL})\n')
+    with open(f'{TEAM_CODE}.md', 'w') as md_file:
+        front_matter(md_file, TEAM, f'labs/{TEAM_CODE}/')
+        md_file.write(f'# {TEAM}\n')
+        md_file.write(desc + '\n')
+        md_file.write(f'### [Current roster](https://querthdp.github.io/awww/labs/{TEAM_CODE}_players)')
--- docs/_docs/lab1/index.md
+++
@@ -1,9 +0,0 @@
----
-title: Laboratory 1 Main Page
-permalink: /labs/home/
-redirect_from: /labs/index.html
----
-
-## Laboratory 1 test home page
-
-I hope this time it will work!
